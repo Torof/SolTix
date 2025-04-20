fb5647bac8a58f4bba668f0aad226ff36c934338
Torof <scottpanam@protonmail.com>
2025-04-20 07:21:28 +0200

    chore: add Solana program scaffolding

---
+++ tests/sol-tix.ts
@@ -0,0 +1,16 @@
+// import * as anchor from "@coral-xyz/anchor";
+// import { Program } from "@coral-xyz/anchor";
+// import { SolTix } from "../target/types/sol_tix";
+
+// describe("sol-tix", () => {
+//   // Configure the client to use the local cluster.
+//   anchor.setProvider(anchor.AnchorProvider.env());
+
+//   const program = anchor.workspace.solTix as Program<SolTix>;
+
+//   it("Is initialized!", async () => {
+//     // Add your test here.
+//     const tx = await program.methods.initialize().rpc();
+//     console.log("Your transaction signature", tx);
+//   });
+// });
